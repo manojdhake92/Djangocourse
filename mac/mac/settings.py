f6e55983cdb3ac5b36611be2594a7994c789ebe0
manoj.dhake <manojdhake92@gmail.com>
2021-06-16 19:12:43 +0530

    Added homepage for shop app and created basic structure for blog app . Also, changed basic things in settings.py

--- mac/mac/settings.py
+++ mac/mac/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mac/mac/views.py
+++ mac/mac/views.py
@@ -1,11 +1,5 @@
-
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'shop/index.html')
-
-
-
+    return HttpResponse("Main Web site Home")
